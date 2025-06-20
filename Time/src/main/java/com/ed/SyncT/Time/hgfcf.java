e8387bbff327ee0fa4012eaf59c5d8f73f9ddfe0
Katuri <kkaturi@galaxe.com>
2025-06-20 12:51:55 +0530

    fd

---
+++ Time/src/main/java/com/ed/SyncT/Time/hgfcf.java
@@ -0,0 +1,12 @@
+package com.ed.SyncT.Time;
+
+public class hgfcf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fxfdgg");
+		
+		System.out.println("fxfdgsdhvfgavsgdvSg");
+	}
+
+}
